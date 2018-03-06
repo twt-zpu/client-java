c114d62bb4a1ce1ecb2a37507fb815e86085ba87
hegeduscs <hegeduscs@gmail.com>
2018-03-06 10:54:28 +0100

    publisher and subscriber module skeletons added

---
+++ ArrowheadPublisher/src/main/java/eu/arrowhead/ArrowheadPublisher/PublisherMain.java
@@ -0,0 +1,9 @@
+package eu.arrowhead.ArrowheadPublisher;
+
+public class PublisherMain {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ ArrowheadSubscriber/src/main/java/eu/arrowhead/ArrowheadSubscriber/SubscriberMain.java
@@ -0,0 +1,9 @@
+package eu.arrowhead.ArrowheadSubscriber;
+
+public class SubscriberMain {
+
+  public static void main(String[] args) {
+
+  }
+
+}
