7dfe98d6be80aface3aa0808d460b5b2ca3d6ad3
uzoltan <zoltan.umlauf@gmail.com>
2018-06-13 16:02:01 +0200

    Consumer class heavily commented, to help understanding

--- consumer/src/main/java/eu/arrowhead/client/consumer/ConsumerMain.java
+++ consumer/src/main/java/eu/arrowhead/client/consumer/ConsumerMain.java
@@ -28,11 +28,109 @@ import org.glassfish.jersey.SslConfigurator;
 public class ConsumerMain {
 
   private static boolean isSecure;
+  private static String orchestratorUrl;
   private static final TypeSafeProperties props = Utility.getProp("app.properties");
 
   public static void main(String[] args) {
+    //Prints the working directory for extra information. Working directory should always contain a config folder with the app.properties file!
     System.out.println("Working directory: " + System.getProperty("user.dir"));
 
+    //Compile the URL for the orchestration request.
+    getOrchestratorUrl(args);
+
+    //Start a timer, to measure the speed of the Core Systems and the provider application system.
+    long startTime = System.currentTimeMillis();
+
+    //Compile the payload, that needs to be sent to the Orchestrator - THIS METHOD SHOULD BE MODIFIED ACCORDING TO YOUR NEEDS
+    ServiceRequestForm srf = compileSRF();
+
+    //Sending the orchestration request and parsing the response
+    String providerUrl = sendOrchestrationRequest(srf);
+
+    //Connect to the provider, consuming its service - THIS METHOD SHOULD BE MODIFIED ACCORDING TO YOUR USE CASE
+    consumeService(providerUrl);
+
+    //Printing out the elapsed time during the orchestration and service consumption
+    long endTime = System.currentTimeMillis();
+    System.out.println("Orchestration and Service consumption response time: " + Long.toString(endTime - startTime));
+  }
+
+  //Compiles the payload for the orchestration request
+  private static ServiceRequestForm compileSRF() {
+    /*
+      ArrowheadSystem: systemName, (address, port, authenticationInfo)
+      Since this Consumer skeleton will not receive HTTP requests (does not provide any services on its own),
+      the address, port and authenticationInfo fields can be set to anything.
+      SystemName can be an arbitrarily chosen name, which makes sense for the use case.
+     */
+    ArrowheadSystem consumer = new ArrowheadSystem("client1", "localhost", 0, "null");
+
+    //You can put any additional metadata you look for in a Service here (key-value pairs)
+    Map<String, String> metadata = new HashMap<>();
+    metadata.put("unit", "celsius");
+    if (isSecure) {
+      //This is a mandatory metadata when using TLS, do not delete it
+      metadata.put("security", "token");
+    }
+    /*
+      ArrowheadService: serviceDefinition (name), interfaces, metadata
+      Interfaces: supported message formats (e.g. JSON, XML, JSON-SenML), a potential provider has to have at least 1 match,
+      so the communication between consumer and provider can be facilitated.
+     */
+    ArrowheadService service = new ArrowheadService("IndoorTemperature", Collections.singletonList("json"), metadata);
+
+    //Some of the orchestrationFlags the consumer can use, to influence the orchestration process
+    Map<String, Boolean> orchestrationFlags = new HashMap<>();
+    //When true, the orchestration store will not be queried for "hard coded" consumer-provider connections
+    orchestrationFlags.put("overrideStore", true);
+    //When true, the Service Registry will ping every potential provider, to see if they are alive/available on the network
+    orchestrationFlags.put("pingProviders", false);
+    //When true, the Service Registry will only providers with the same exact metadata map as the consumer
+    orchestrationFlags.put("metadataSearch", true);
+    //When true, the Orchestrator can turn to the Gatekeeper to initiate interCloud orchestration, if the Local Cloud had no adequate provider
+    orchestrationFlags.put("enableInterCloud", true);
+
+    //Build the complete service request form from the pieces, and return it
+    ServiceRequestForm srf = new ServiceRequestForm.Builder(consumer).requestedService(service).orchestrationFlags(orchestrationFlags).build();
+    System.out.println("Service Request payload: " + Utility.toPrettyJson(null, srf));
+    return srf;
+  }
+
+  private static void consumeService(String providerUrl) {
+    /*
+      Sending request to the provider, to the acquired URL. The method type and payload should be known beforehand.
+      If needed, compile the request payload here, before sending the request.
+      Supported method types at the moment: GET, POST, PUT, DELETE
+     */
+    Response getResponse = Utility.sendRequest(providerUrl, "GET", null);
+
+    /*
+      Parsing the response from the provider here. This code prints an error message, if the answer is not in the expected JSON format, but custom
+      error handling can also be implemented here. For example the Orchestrator will send back a JSON with the structure of the eu.arrowhead.client
+      .common.exception.ErrorMessage class, and the errors from the Orchestrator are parsed this way.
+     */
+    TemperatureReadout readout = new TemperatureReadout();
+    try {
+      readout = getResponse.readEntity(TemperatureReadout.class);
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+      System.out.println("Provider did not send the temperature readout in SenML format.");
+    }
+    if (readout.getE().get(0) == null) {
+      System.out.println("Provider did not send any MeasurementEntry.");
+    } else {
+      System.out.println("The indoor temperature is " + readout.getE().get(0).getV() + " degrees celsius.");
+    }
+  }
+
+   /*
+      Methods that should be modified to your use case ↑
+   ----------------------------------------------------------------------------------------------------------------------------------
+      Methods that do not need to be modified ↓
+   */
+
+  //DO NOT MODIFY - Gets the correct URL where the orchestration requests needs to be sent (from app.properties config file + command line argument)
+  private static void getOrchestratorUrl(String[] args) {
     String orchAddress = props.getProperty("orch_address", "0.0.0.0");
     int orchInsecurePort = props.getIntProperty("orch_insecure_port", 8440);
     int orchSecurePort = props.getIntProperty("orch_secure_port", 8441);
@@ -50,29 +148,29 @@ public class ConsumerMain {
       }
     }
 
-    String ORCH_URI;
     if (isSecure) {
-      ORCH_URI = Utility.getUri(orchAddress, orchSecurePort, "orchestrator/orchestration", true, false);
+      orchestratorUrl = Utility.getUri(orchAddress, orchSecurePort, "orchestrator/orchestration", true, false);
     } else {
-      ORCH_URI = Utility.getUri(orchAddress, orchInsecurePort, "orchestrator/orchestration", false, false);
+      orchestratorUrl = Utility.getUri(orchAddress, orchInsecurePort, "orchestrator/orchestration", false, false);
     }
+  }
 
-    long startTime = System.currentTimeMillis();
-
-    //Payload compiling
-    ServiceRequestForm srf = compileSRF();
-    System.out.println("Service Request payload: " + Utility.toPrettyJson(null, srf));
-
-    //Sending request to the orchestrator, parsing the response
-    Response postResponse = Utility.sendRequest(ORCH_URI, "POST", srf);
+  /* NO NEED TO MODIFY (for basic functionality)
+     Sends the orchestration request to the Orchestrator, and compiles the URL for the first provider received from the OrchestrationResponse */
+  private static String sendOrchestrationRequest(ServiceRequestForm srf) {
+    //Sending a POST request to the orchestrator (URL, method, payload)
+    Response postResponse = Utility.sendRequest(orchestratorUrl, "POST", srf);
+    //Parsing the orchestrator response
     OrchestrationResponse orchResponse = postResponse.readEntity(OrchestrationResponse.class);
     System.out.println("Orchestration Response payload: " + Utility.toPrettyJson(null, orchResponse));
-
     if (orchResponse.getResponse().isEmpty()) {
       throw new ArrowheadException("Orchestrator returned with 0 Orchestration Forms!");
     }
+
+    //Getting the first provider from the response
     ArrowheadSystem provider = orchResponse.getResponse().get(0).getProvider();
     String serviceURI = orchResponse.getResponse().get(0).getServiceURI();
+    //Compiling the URL for the provider
     UriBuilder ub = UriBuilder.fromPath("").host(provider.getAddress()).scheme("http");
     if (serviceURI != null) {
       ub.path(serviceURI);
@@ -86,41 +184,7 @@ public class ConsumerMain {
       ub.queryParam("signature", orchResponse.getResponse().get(0).getSignature());
     }
     System.out.println("Received provider system URL: " + ub.toString());
-
-    //Sending request to the provider, parsing the answer
-    Response getResponse = Utility.sendRequest(ub.toString(), "GET", null);
-    TemperatureReadout readout = new TemperatureReadout();
-    try {
-      readout = getResponse.readEntity(TemperatureReadout.class);
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      System.out.println("Provider did not send the temperature readout in SenML format.");
-    }
-    if (readout.getE().get(0) == null) {
-      System.out.println("Provider did not send any MeasurementEntry.");
-    } else {
-      long endTime = System.currentTimeMillis();
-      System.out.println("The indoor temperature is " + readout.getE().get(0).getV() + " degrees celsius.");
-      System.out.println("Orchestration and Service consumption response time: " + Long.toString(endTime - startTime));
-    }
+    return ub.toString();
   }
 
-  private static ServiceRequestForm compileSRF() {
-    ArrowheadSystem consumer = new ArrowheadSystem("client1", "localhost", 0, "null");
-
-    Map<String, String> metadata = new HashMap<>();
-    metadata.put("unit", "celsius");
-    if (isSecure) {
-      metadata.put("security", "token");
-    }
-    ArrowheadService service = new ArrowheadService("IndoorTemperature", Collections.singletonList("json"), metadata);
-
-    Map<String, Boolean> orchestrationFlags = new HashMap<>();
-    orchestrationFlags.put("overrideStore", true);
-    orchestrationFlags.put("pingProviders", false);
-    orchestrationFlags.put("metadataSearch", true);
-    orchestrationFlags.put("enableInterCloud", true);
-
-    return new ServiceRequestForm.Builder(consumer).requestedService(service).orchestrationFlags(orchestrationFlags).build();
-  }
 }
