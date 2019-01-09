335174218bef7238e97f3377e124670f739aec08
Thomas Pedersen <tp@cs.aau.dk>
2019-01-09 17:38:25 +0100

    Moved buildURI methods into Utility class...

--- client-common/src/main/java/eu/arrowhead/common/api/clients/OrchestrationStrategy.java
+++ client-common/src/main/java/eu/arrowhead/common/api/clients/OrchestrationStrategy.java
@@ -2,13 +2,16 @@ package eu.arrowhead.common.api.clients;
 
 import eu.arrowhead.common.api.clients.core.OrchestrationClient;
 import eu.arrowhead.common.exception.DataNotFoundException;
-import eu.arrowhead.common.model.*;
-import org.apache.log4j.Logger;
-
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
+import eu.arrowhead.common.misc.Utility;
+import eu.arrowhead.common.model.OrchestrationForm;
+import eu.arrowhead.common.model.OrchestrationResponse;
+import eu.arrowhead.common.model.ServiceMetadata;
+import eu.arrowhead.common.model.ServiceRequestForm;
 import java.net.URI;
 import java.util.Set;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import org.apache.log4j.Logger;
 
 /**
  * Orchestration strategies define how and when orchestration happens when using {@link HttpClient}.
@@ -33,59 +36,6 @@ public abstract class OrchestrationStrategy {
      * @return the response.
      */
     public abstract Response request(HttpClient client, HttpClient.Method method, UriBuilder appendUri, Object payload);
-
-    /**
-     * Build an URI on the append URI, given the raw elements. This does not add security token.
-     * @param appendUri the append URI.
-     * @param secure secure mode (e.g. use HTTP or HTTPS)?
-     * @param address the host.
-     * @param port the port.
-     * @param serviceURI the URI of the service (will be added before any path already present in the append URI.
-     * @return the append URI.
-     */
-    protected UriBuilder buildUri(UriBuilder appendUri, boolean secure, String address, Integer port, String serviceURI) {
-        appendUri.scheme(secure ? "https" : "http")
-                .host(address);
-        if (serviceURI != null) {
-            final String path = appendUri.build().getPath();
-            appendUri.replacePath(serviceURI).path(path);
-        }
-        if (port != null) appendUri.port(port);
-        return appendUri;
-    }
-
-    /**
-     * Add a token and signature elements to the uri builder.
-     * @param uriBuilder the uri builder.
-     * @param authorizationToken the token.
-     * @param signature the signature.
-     * @return the uri builder.
-     */
-    protected UriBuilder addToken(UriBuilder uriBuilder, String authorizationToken, String signature) {
-        uriBuilder
-                .queryParam("token", authorizationToken)
-                .queryParam("signature", signature);
-        return uriBuilder;
-    }
-
-    /**
-     * Build an URI on the append URI, given an orchestration entry. This does add security token if the entry requires
-     * secure mode.
-     * @param appendUri the append URI.
-     * @param entry the orchestration entry, and given by the {@link OrchestrationClient}.
-     * @return the append URI.
-     */
-    protected UriBuilder buildUri(UriBuilder appendUri, OrchestrationForm entry) {
-        final ArrowheadSystem provider = entry.getProvider();
-        final boolean secure = entry.isSecure();
-        final String address = provider.getAddress();
-        final Integer port = provider.getPort();
-        final String serviceURI = entry.getServiceURI();
-
-        appendUri = buildUri(appendUri, secure, address, port, serviceURI);
-        if (secure) appendUri = addToken(appendUri, entry.getAuthorizationToken(), entry.getSignature());
-        return appendUri;
-    }
 
     /**
      * Should return true if the strategy runs requests in secure mode.
@@ -149,7 +99,7 @@ public abstract class OrchestrationStrategy {
 
         @Override
         public Response request(HttpClient client, HttpClient.Method method, UriBuilder appendUri, Object payload) {
-            final URI uri = buildUri(appendUri, secure, address, port, serviceUri).build();
+            final URI uri = Utility.buildUri(appendUri, secure, address, port, serviceUri).build();
             return send(client, uri, method, anInterface, payload);
         }
 
@@ -183,7 +133,7 @@ public abstract class OrchestrationStrategy {
 
         @Override
         public Response request(HttpClient client, HttpClient.Method method, UriBuilder appendUri, Object payload) {
-            final URI uri = buildUri(appendUri, entry).build();
+            final URI uri = Utility.buildUri(appendUri, entry).build();
             final Set<String> interfaces = entry.getService().getInterfaces();
             return send(client, uri, method, interfaces, payload);
         }
@@ -217,7 +167,7 @@ public abstract class OrchestrationStrategy {
                 // TODO Should add a loop here !!!
                 final OrchestrationResponse response1 = orchestrationClient.request(serviceRequestForm);
                 final OrchestrationForm entry = response1.getFirst();
-                final URI uri = buildUri(appendUri, entry).build();
+                final URI uri = Utility.buildUri(appendUri, entry).build();
                 final Set<String> interfaces = entry.getService().getInterfaces();
                 return send(client, uri, method, interfaces, payload);
             } catch (DataNotFoundException e) {
--- client-common/src/main/java/eu/arrowhead/common/misc/Utility.java
+++ client-common/src/main/java/eu/arrowhead/common/misc/Utility.java
@@ -11,15 +11,22 @@ package eu.arrowhead.common.misc;
 
 import eu.arrowhead.common.api.ArrowheadConverter;
 import eu.arrowhead.common.exception.ArrowheadRuntimeException;
-
-import javax.ws.rs.core.UriBuilder;
-import java.io.*;
+import eu.arrowhead.common.model.ArrowheadSystem;
+import eu.arrowhead.common.model.OrchestrationForm;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ServiceConfigurationError;
+import javax.ws.rs.core.UriBuilder;
 
 //Contains static utility methods for the project, most important one is the sendRequest method!
 public final class Utility {
@@ -132,4 +139,57 @@ public final class Utility {
         return (str == null || "".equals(str.trim()));
     }
 
+  /**
+   * Build an URI on the append URI, given the raw elements. This does not add security token.
+   * @param appendUri the append URI.
+   * @param secure secure mode (e.g. use HTTP or HTTPS)?
+   * @param address the host.
+   * @param port the port.
+   * @param serviceURI the URI of the service (will be added before any path already present in the append URI.
+   * @return the append URI.
+   */
+  public static UriBuilder buildUri(UriBuilder appendUri, boolean secure, String address, Integer port,
+                                    String serviceURI) {
+      appendUri.scheme(secure ? "https" : "http")
+              .host(address);
+      if (serviceURI != null) {
+          final String path = appendUri.build().getPath();
+          appendUri.replacePath(serviceURI).path(path);
+      }
+      if (port != null) appendUri.port(port);
+      return appendUri;
+  }
+
+  /**
+   * Add a token and signature elements to the uri builder.
+   * @param uriBuilder the uri builder.
+   * @param authorizationToken the token.
+   * @param signature the signature.
+   * @return the uri builder.
+   */
+  public static UriBuilder addToken(UriBuilder uriBuilder, String authorizationToken, String signature) {
+      uriBuilder
+              .queryParam("token", authorizationToken)
+              .queryParam("signature", signature);
+      return uriBuilder;
+  }
+
+  /**
+   * Build an URI on the append URI, given an orchestration entry. This does add security token if the entry requires
+   * secure mode.
+   * @param appendUri the append URI.
+   * @param entry the orchestration entry, and given by the {@link eu.arrowhead.common.api.clients.core.OrchestrationClient}.
+   * @return the append URI.
+   */
+  public static UriBuilder buildUri(UriBuilder appendUri, OrchestrationForm entry) {
+      final ArrowheadSystem provider = entry.getProvider();
+      final boolean secure = entry.isSecure();
+      final String address = provider.getAddress();
+      final Integer port = provider.getPort();
+      final String serviceURI = entry.getServiceURI();
+
+      appendUri = buildUri(appendUri, secure, address, port, serviceURI);
+      if (secure) appendUri = addToken(appendUri, entry.getAuthorizationToken(), entry.getSignature());
+      return appendUri;
+  }
 }
