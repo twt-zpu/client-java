ab63b11d103c6215dbf603a48fca990f09c02dc6
Thomas Pedersen <tp@cs.aau.dk>
2019-04-17 15:27:00 +0200

    Updated and released Archetypes in repository for testing

--- release.sh
+++ release.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+set -ex
+
+REPO=$(pwd)
+DIR="$(pwd)/$(dirname "$0")"
+
 mvn install:install-file -DgroupId=eu.arrowhead -DartifactId=arrowhead-client-common -Dversion=4.1.1-SNAPSHOT \
-    -Dfile=$(dirname "$0")/client-common/target/arrowhead-client-common-4.1.1-SNAPSHOT.jar -Dpackaging=jar \
-    -DgeneratePom=true -DlocalRepositoryPath=$(pwd) -DcreateChecksum=true
+    -Dfile=${DIR}/client-common/target/arrowhead-client-common-4.1.1-SNAPSHOT.jar -Dpackaging=jar \
+    -DgeneratePom=true -DlocalRepositoryPath=${REPO} -DcreateChecksum=true
+
+for d in project common application
+do
+  cd ${DIR}/archetype/${d}
+  mvn deploy -DaltDeploymentRepository=snapshot-repo::default::file:${REPO}
+done
