3c74037e182ae8760ffce97bde4116f6387ad8aa
Thomas Pedersen <tp@cs.aau.dk>
2019-04-23 12:06:21 +0200

    Feature: Delete entire archetype dir before doing a new release

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ set -e
 REPO=$(pwd)
 DIR="$(pwd)/$(dirname "$0")"
 
+rm -r eu/arrowhead/archetype
+
 mvn install:install-file -DgroupId=eu.arrowhead -DartifactId=arrowhead-client-common -Dversion=4.1.3-SNAPSHOT \
     -Dfile=${DIR}/client-common/target/arrowhead-client-common-4.1.3-SNAPSHOT.jar -Dpackaging=jar \
     -DgeneratePom=true -DlocalRepositoryPath=${REPO} -DcreateChecksum=true
